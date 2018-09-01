011a7bb2d21adc720f7666d8c843485cac75f88c
hoadq <hoadinhquoc@gmail.com>
2018-09-01 19:25:11 +0700

    +Init prj

---
+++ Assets/Scripts/Runner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runner : MonoBehaviour {
+
+	[SerializeField] Vector3 VelocityVector;
+	[SerializeField] float JumpHeight = 5f;
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
